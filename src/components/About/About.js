f21b03b7daff64c5794968d0dad80b62aa592820
Jayraj Dulange <jayraj.jayraj@iitgn.ac.in>
2024-02-04 06:09:40 +0530

    updated home and about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,19 +1,30 @@
 import "./About.css";
+import React, { useEffect } from "react";
 import Plx from "react-plx";
 function About(){
+    useEffect(() => {
+        const s = window.skrollr.init();
+    
+        return () => {
+          s.destroy();
+        };
+      }, []);
     return (
         <>
         <div className="about-container">
-            <img src="Vector2.svg"></img>
+            <img data-600="opacity: 0;" data-1000="opacity: 1;" src="Vector2.svg"></img>
             <div className="info">
-                <h1 id="about">About</h1>
-                <span id="ted">
+                <h1 data-600="opacity: 0;" data-1000="opacity: 1;" data-1200="opacity: 0;" id="about">About TED</h1>
+                <span data-600="opacity: 0;" data-1000="opacity: 1;" data-1200="opacity: 0;" id="ted">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc.
                 </span>
+                {/* <h1 data-1200="opacity: 0;" data-1700="opacity: 1;" id="about">About TEDx</h1>
+                <span data-1200="opacity: 0;" data-1700="opacity: 1;" id="ted">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, quis aliquam nisl nisl vitae nunc.
+                </span> */}
             </div>
         </div>
         </>
-
     )
 };
 
