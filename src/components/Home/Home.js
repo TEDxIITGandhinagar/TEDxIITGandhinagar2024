0039db8a02686f46778cb7a317c4b8cd345a7dce
Jayraj Dulange <jayraj.jayraj@iitgn.ac.in>
2024-03-18 21:45:27 +0530

    most recent update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,24 +2,37 @@ import About from "../About/About";
 import './Home.css'
 // import HorizontalScrollGallery from "./memory";
 import MainPageVideobg from "../MainPageVideobg";
-import CountdownTimer from "./Countdown/Countdown";
+
 function Home(){
 
     return(
         <>
         <MainPageVideobg/>
 
+        <div className="release">
         <div className="suspense">
             <h1>Revealing The Theme of</h1>
         </div>
-
+        
         <div className="tubelight">
             <h1>
-                <span className="red-ted">TEDX</span> <span className="iitgn">IITGANDHINAGAR 2024</span>
+                <span className="red-ted">T
+                <span className="flick d1">E</span>D
+                <span className="flick d2">X</span>
+                </span> <span className="iitgn">IIT
+                <span className="flick d3">G</span>ANDHI
+                <span className="flick d4">N</span>AGA
+                <span className="flick d5">R</span> 202
+                <span className="flick d6">4</span>
+                </span>
             </h1>
         </div>
+        </div>
+        <div className="theme">
 
-        <CountdownTimer/>
+            <h1 id="theme-text">Reimagining Reality.</h1>
+        </div>
+
         <div id="about-section">
             <About/>
         </div>
