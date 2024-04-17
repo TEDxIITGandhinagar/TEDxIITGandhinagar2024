fbb69867c5a9ace0c68e6140e71ae35ead6fe16d
Jayraj Dulange <jayraj.jayraj@iitgn.ac.in>
2024-04-17 19:15:58 +0530

    optimised few parts

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,5 @@
 import "./About.css";
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 
 function About(){
 
--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./Sponsors.css";
 import { TitleSponsData, PlatSponsData, GoldSponsData, DiamSponsData,  CoSponsData, PartnerData } from "./SponsData";
 
